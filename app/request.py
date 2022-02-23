48d7aefa626a91874612ffc00073de90361557ed
verowangari <verowangari34@gmail.com>
2022-02-23 20:25:50 +0300

    call app.config to access the configuration objects

---
+++ app/request.py
@@ -0,0 +1,10 @@
+from app import app
+import urllib.request,json
+from .models import movie
+
+Movie=movie.Movie
+# Getting api key
+api_key = app.config['MOVIE_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["MOVIE_API_BASE_URL"]
