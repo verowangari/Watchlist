d85b04375944d0e62b3ddf9b2feb565aed0ba169
verowangari <verowangari34@gmail.com>
2022-02-23 20:33:00 +0300

    create a function that takes in a movie category as an argument

--- app/request.py
+++ app/request.py
@@ -13,3 +13,16 @@ def get_movies(category):
     '''
     Function that gets the json response to our url request
     '''
+    get_movies_url=base_url.format(category,api_key)
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results = None
+
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+            movie_results = process_results(movie_results_list)
+
+
+    return movie_results
