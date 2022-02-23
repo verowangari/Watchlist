0bec180085db479f924d5aa2f91e4a6ba64b4ad6
verowangari <verowangari34@gmail.com>
2022-02-23 23:23:39 +0300

    initialize boostsrap

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,10 @@ app = Flask(__name__, instance_relative_config=True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile("config.py")
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
 
 from app import views
