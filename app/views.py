b495873cdfb9ffd1f93036607682e35ac9061ecb
verowangari <verowangari34@gmail.com>
2022-02-23 14:36:29 +0300

    pass in a title to our template

--- app/views.py
+++ app/views.py
@@ -1,12 +1,16 @@
 from email import message
+from turtle import title
 from flask import render_template
 from app import app
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+
+
+@app.route('/')
+def index():
 
     '''
-    View movie page function that returns the movie details page and its data
-   '''
-    
-    return render_template('movie.html',id=movie_id)
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
 
