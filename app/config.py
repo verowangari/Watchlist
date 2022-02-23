d7337f60612742fdd2994410d72b3ab1a469a671
verowangari <verowangari34@gmail.com>
2022-02-23 13:42:57 +0300

    create a development configuration class

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
+from distutils.debug import DEBUG
+from inspect import Arguments
+
+
 class Config:
     '''
     General configuration parent class
@@ -6,4 +10,15 @@ class Config:
 class prodConfig(Config):
     '''
     Production configuration child class
+       Args:
+        Config: The parent configuration class with General configuration settings
     '''
+    pass
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+     Config: The parent configuration class with General configurations settings
+    '''
+    DEBUG=True
+  
