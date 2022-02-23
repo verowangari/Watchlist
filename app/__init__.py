4c3dced49f891da406a0cf49fa21557aa82e9bbd
verowangari <verowangari34@gmail.com>
2022-02-23 14:26:24 +0300

    remove debug argument from run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
 from flask import Flask
+from .config import DevConfig
+#Initializing the application
 app=Flask(__name__)
+#Setting up configuration
+app.config.from_object(DevConfig)
 from app import views
