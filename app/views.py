b185c3b1e8d70163386f894f9faad746476ba3bf
verowangari <verowangari34@gmail.com>
2022-02-25 19:03:12 +0300

    create a search view function that passes in a dynamic variable

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
 from .request import get_movies, get_movie
-
+from .request import get_movies,get_movie,search_movie
 
 # Views
 @app.route('/')
@@ -27,3 +27,13 @@ def movie(id):
     title = f'{movie.title}'
 
     return render_template('movie.html', title=title, movie=movie)
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
